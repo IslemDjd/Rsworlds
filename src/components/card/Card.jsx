a9d282609a3453038fca07f97b2e01b0a4a22c95
Islem Dj <islemas23@gmail.com>
2024-03-09 17:14:38 +0100

    .

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 import Button from "../button/Button";
 import "./card.scss";
-import icon from '../../assets/cart.svg'
+import icon from "../../assets/cart.svg";
+import { Link } from "react-router-dom";
 
 const Card = (props) => {
   return (
     <>
-      <a href={`/articles/${props.articleId}`}>
+      <Link to={`/articles/${props.articleId}`}>
         <div className="card">
           <div className="image_container">
             <img className="image" src={props.img} alt="" />
@@ -20,10 +21,15 @@ const Card = (props) => {
                 Size:{props.size} - {props.price}DA
               </span>
             </div>
-            <Button image={icon} width="100%" margin="0 0.5rem 0.5rem 0.5rem" text="Add to cart" />
+            <Button
+              image={icon}
+              width="100%"
+              margin="0 0.5rem 0.5rem 0.5rem"
+              text="Add to cart"
+            />
           </div>
         </div>
-      </a>
+      </Link>
     </>
   );
 };
